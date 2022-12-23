fc0b93d5ba7b139c152931b5cae6e961f184d7e7
Yuri Meshalkin <Yuri.Meshalkin@gmail.com>
2022-12-23 15:48:23 +0200

    update to 7.1.4.1

--- build/config_arnold-usd_v7.1.4.1_22.11_py3.sh
+++ build/config_arnold-usd_v7.1.4.1_22.11_py3.sh
@@ -6,6 +6,9 @@ RMAN_VER="24.4"
 
 PYTHON_VERSION="3"
 
+echo "* source env_python3.9/bin/activate"
+source ./env_python3.9/bin/activate
+
 app_name="arnold-usd"
 app_ver="7.1.4.1"
 
@@ -23,6 +26,7 @@ export PXR_USD_LOCATION="${deploy_root}/USD/pixar/USD-v${USD_VER}_rman${RMAN_VER
 export BOOST_ROOT="${deploy_root}/boost/boost_1_75_0_ABI_0"
 export Boost_DIR="${BOOST_ROOT}/lib/cmake/Boost-1.75.0"
 export BOOST_LIBRARYDIR=${BOOST_ROOT}
+export MATERIALX_ROOT="${deploy_root}/MaterialX/MaterialX-v1.38.5_ABI_0"
 
 #export MTOA_LOCATION="/opt/solidangle/mtoa/mtoa-4.0.4/2020"
 #export ARNOLD_LOCATION="/opt/solidangle/Arnold-6.0.4.1-linux"
@@ -39,29 +43,33 @@ cmake3 -LA -G "Unix Makefiles" \
 -DTBB_STATIC_BUILD=OFF \
 -DTBB_NO_EXPLICIT_LINKAGE=OFF \
 -DBUILD_USE_CUSTOM_BOOST=OFF \
+-DBUILD_BOOST_ALL_NO_LIB=OF \
 -DBUILD_DISABLE_CXX11_ABI=ON \
 -DBUILD_HEADERS_AS_SOURCES=OFF \
--DBUILD_SCHEMAS=OFF \
+-DBUILD_SCHEMAS=ON \
 -DBUILD_RENDER_DELEGATE=ON \
 -DBUILD_SCENE_DELEGATE=ON \
--DBUILD_PROC_SCENE_FORMAT=ON \
 -DBUILD_NDR_PLUGIN=ON \
 -DBUILD_PROCEDURAL=ON \
+-DBUILD_PROC_SCENE_FORMAT=ON \
 -DBUILD_USD_WRITER=ON \
 -DBUILD_USD_IMAGING_PLUGIN=ON \
 -DBUILD_DOCS=OFF \
 -DBUILD_TESTSUITE=OFF \
 -DBUILD_UNIT_TESTS=OFF \
 -DBUILD_USE_PYTHON3=ON \
+-DENABLE_MATERIALX=ON \
 -DUSD_LOCATION=${PXR_USD_LOCATION} \
 -DARNOLD_LOCATION=${ARNOLD_LOCATION} \
--DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" \
--DCMAKE_CXX_STANDARD="14" \
--DBOOST_ROOT=${BOOST_ROOT} \
 -DBoost_DIR=${Boost_DIR} \
 ../..
 
 cd ..
+
+# -DCMAKE_CXX_STANDARD="14" \
+
+# BUILD_DISABLE_CXX11_ABI solve this:
+# -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" \
 
 # -DTBB_INCLUDE_DIR="${TBBROOT}/include" \
 # -DOpenGL_GL_PREFERENCE=GLVND \
@@ -83,3 +91,4 @@ else
   echo "* "
 fi
 
+# deactivate
